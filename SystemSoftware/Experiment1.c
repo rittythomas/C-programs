cd6cb4867a27dfef768f5bd1dda7c4491399a003
Ritty Thomas <37534943+rittythomas@users.noreply.github.com>
2019-08-17 17:02:34 +0530

    Update Experiment1.c

--- SystemSoftware/Experiment1.c
+++ SystemSoftware/Experiment1.c
@@ -1,7 +1,6 @@
 #include<stdio.h>
-#include<conio.h>
 
-##DONE ONLY FCFS. SJS AND PRIORITY LEFT.
+
 
 void findWaitingTime(int n, int bt[], int wt[])
 {
@@ -19,7 +18,6 @@ void findTurnAroundTime(int n, int bt[], int wt[], int tat[])
 }
 void FCFSAlgorithm(int n, int bt[],int wt[], int tat[])
 {
-	printf("FCFS");
 	findWaitingTime(n,bt,wt);
 	findTurnAroundTime(n,bt,wt,tat);
 	printf("P BT WT TAT\n");
@@ -32,19 +30,100 @@ void FCFSAlgorithm(int n, int bt[],int wt[], int tat[])
 		printf("\n");
 	}
 }
+void SJF(int n,int bt[],int wt[],int tat[])
+{
+		int i,j,temp;
+		for(i = 0;i < n; i++){
+			int pos = i;
+			for(j = i+1; j < n; j++){
+				if(bt[j] < bt[pos]){
+					pos = j;
+				}
+				temp = bt[i];
+				bt[i] = bt[pos];
+				bt[pos] = temp;
+			}
+		}
+}
+void Priority(int n,int bt[],int wt[], int tat[], int pr[],int p[]){
+	int i,j,temp, pos,total;
+	for(i=0;i<n;i++)
+	 {
+	     
+			 pos=i;
+			 for(j=i+1;j<n;j++)
+			 {
+					 if(pr[j]<pr[pos])
+							 pos=j;
+			 }
+
+			 temp=pr[i];
+			 pr[i]=pr[pos];
+			 pr[pos]=temp;
+
+			 temp=bt[i];
+			 bt[i]=bt[pos];
+			 bt[pos]=temp;
+
+			 temp=p[i];
+			 p[i]=p[pos];
+			 p[pos]=temp;
+	 }
+	 wt[0]=0;	//waiting time for first process is zero
+
+	 //calculate waiting time
+	 for(i=1;i<n;i++)
+	 {
+			 wt[i]=0;
+			 for(j=0;j<i;j++)
+					 wt[i]+=bt[j];
+
+		     total+=wt[i];
+	 }
+
+	 printf("\nProcess\t    Burst Time    \tWaiting Time\tTurnaround Time");
+	 for(i=0;i<n;i++)
+	 {
+			 tat[i]=bt[i]+wt[i];     //calculate turnaround time
+			 total+=tat[i];
+			 printf("\nP[%d]\t\t  %d\t\t    %d\t\t\t%d",p[i],bt[i],wt[i],tat[i]);
+	 }
+
+}
+
 void main()
 {
 
-	int n,i, tat[20],wt[20],bt[20];
-	clrscr();
+	int ch,n,i, tat[20],wt[20],bt[20],p[20],pr[20];
+
 	printf("Enter number of process:");
 	scanf("%d",&n);
-	printf("\n Enter process Burst time:\n");
+	printf("\n Enter process Burst time & Priority:\n");
 	for(i=0 ; i<n; i++)
 	{
 		printf("P[%d]:",i+1);
-		scanf("%d", &bt[i]);
+		printf("Burst Time:");
+			 scanf("%d",&bt[i]);
+			 printf("Priority:");
+			 scanf("%d",&pr[i]);
+			 p[i]=i+1; //process number
 	}
-	FCFSAlgorithm(n,bt,wt,tat);
+	printf("Stimulate:\n 1.FCFSAlgorithm\n 2.SFF\n 3.Priority\n 4.Round Robin\n");
+	scanf("%d",&ch);
+	switch(ch)
+	{
+		case 1:
+					FCFSAlgorithm(n,bt,wt,tat);
+					break;
+		case 2:
+					SJF(n,bt,wt,tat);
+					break;
+		case 3:
+					Priority(n,bt,wt,tat,pr,p);
+					break;
+		default:
+					printf("Wrong input\n");
+	}
 	getch();
 }
+
